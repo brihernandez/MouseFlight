aaa8101943f4e318ba98b5489e4a2f986813ccc9
Brian <bmh123@comcast.net>
2018-12-31 00:58:47 -0500

    Initial commit

---
+++ Assets/MouseFlight/Demo/Scripts/Hud.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace MFlight.Demo
+{
+    public class Hud : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private MouseFlightController mouseFlight = null;
+
+        [Header("HUD Elements")]
+        [SerializeField] private RectTransform boresight = null;
+        [SerializeField] private RectTransform mousePos = null;
+
+        private void Update()
+        {
+            if (mouseFlight == null)
+                return;
+
+            UpdateGraphics();
+        }
+
+        private void UpdateGraphics()
+        {
+            if (boresight != null)
+            {
+                boresight.position = Camera.main.WorldToScreenPoint(mouseFlight.BoresightPos);
+                //boresight.gameObject.SetActive(boresight.position.z > 1f);
+            }
+
+            if (mousePos != null)
+            {
+                mousePos.position = Camera.main.WorldToScreenPoint(mouseFlight.MouseAimPos);
+                //mousePos.gameObject.SetActive(boresight.position.z > 1f);
+            }
+        }
+    }
+}
---
+++ Assets/MouseFlight/Scripts/MouseFlightController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+
+namespace MFlight
+{
+    /// <summary>
+    /// Combination of camera rig and controller for aircraft. Requires a properly set
+    /// up rig. I highly recommend using the included prefab so that the hierarchy is
+    /// correct. 
+    /// </summary>
+    public class MouseFlightController : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private Transform aircraft = null;
+        [SerializeField] private Transform mouseAim = null;
+        [SerializeField] private Transform cameraRig = null;
+        [SerializeField] private Transform cam = null;
+
+        [Header("Options")]
+        [SerializeField] [Tooltip("Follow aircraft using fixed update loop")]
+        private bool useFixed = true;
+
+        [SerializeField] [Tooltip("How quickly the camera tracks the mouse aim point.")]
+        private float camSmoothSpeed = 5f;
+
+        [SerializeField] [Tooltip("Mouse sensitivity for the mouse flight target")]
+        private float mouseSensitivity = 3f;
+
+        [SerializeField] [Tooltip("How far the boresight and mouse flight are from the aircraft")]
+        private float aimDistance = 500f;
+
+        /// <summary>
+        /// Get a point along the aircraft's boresight projected out to aimDistance meters.
+        /// Useful for drawing a crosshair to aim fixed forward guns with, or to indicate what
+        /// direction the aircraft is pointed.
+        /// </summary>
+        public Vector3 BoresightPos
+        {
+            get { return (aircraft.transform.forward * aimDistance) + aircraft.transform.position; }
+        }
+
+        /// <summary>
+        /// Get the position that the mouse is indicating the aircraft should fly, projected
+        /// out to aimDistance meters. Also meant to be used to draw a mouse cursor.
+        /// </summary>
+        public Vector3 MouseAimPos
+        {
+            get { return (mouseAim.transform.forward * aimDistance) + mouseAim.transform.position; }
+        }
+
+        private void Update()
+        {
+            if (useFixed == false)
+                UpdateCameraPos();
+
+            RotateRig();
+        }
+
+        private void FixedUpdate()
+        {
+            if (useFixed == true)
+                UpdateCameraPos();
+        }
+
+        private void RotateRig()
+        {
+            // Mouse input.
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float mouseY = -Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            // Rotate the aim target that the plane is meant to fly towards.
+            // Use the camera's axes in world space so that mouse motion is intuitive.
+            mouseAim.Rotate(cam.right, mouseY, Space.World);
+            mouseAim.Rotate(cam.up, mouseX, Space.World);
+
+            // The up vector of the camera normally is aligned to the horizon. However, when
+            // looking straight up/down this can feel a bit weird. At those extremes, the camera
+            // stops aligning to the horizon and instead aligns to itself.
+            Vector3 upVec = (Mathf.Abs(mouseAim.forward.y) > 0.9f) ? cameraRig.up : Vector3.up;
+
+            // Smoothly rotate the camera to face the mouse aim.
+            cameraRig.rotation = Damp(cameraRig.rotation,
+                                      Quaternion.LookRotation(MouseAimPos - cameraRig.position, upVec),
+                                      camSmoothSpeed,
+                                      Time.deltaTime);
+        }
+
+        private void UpdateCameraPos()
+        {
+            transform.position = aircraft.position;
+        }
+
+        // Thanks to Rory Driscoll
+        // http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+        /// <summary>
+        /// Creates dampened motion between a and b that is framerate independent.
+        /// </summary>
+        /// <param name="a">Initial parameter</param>
+        /// <param name="b">Target parameter</param>
+        /// <param name="lambda">Smoothing factor</param>
+        /// <param name="dt">Time since last damp call</param>
+        /// <returns></returns>
+        private Quaternion Damp(Quaternion a, Quaternion b, float lambda, float dt)
+        {
+            return Quaternion.Slerp(a, b, 1 - Mathf.Exp(-lambda * dt));
+        }
+    }
+}
