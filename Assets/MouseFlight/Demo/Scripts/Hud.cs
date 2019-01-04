f987a93da50c79b2876730f7616c326a27da4070
Brian <bmh123@comcast.net>
2019-01-04 13:21:52 -0500

    Hud derives camera from the MouseFlightController

--- Assets/MouseFlight/Demo/Scripts/Hud.cs
+++ Assets/MouseFlight/Demo/Scripts/Hud.cs
@@ -11,9 +11,22 @@ namespace MFlight.Demo
         [SerializeField] private RectTransform boresight = null;
         [SerializeField] private RectTransform mousePos = null;
 
-        private void Update()
+        private Camera playerCam = null;
+
+        private void Awake()
         {
             if (mouseFlight == null)
+                Debug.LogError($"{name}: Hud - Mouse Flight Controller not assigned!");
+
+            playerCam = mouseFlight.GetComponentInChildren<Camera>();
+
+            if (playerCam == null)
+                Debug.LogError($"{name}: Hud - No camera found on assigned Mouse Flight Controller!");
+        }
+
+        private void Update()
+        {
+            if (mouseFlight == null || playerCam == null)
                 return;
 
             UpdateGraphics(mouseFlight);
@@ -23,13 +36,13 @@ namespace MFlight.Demo
         {
             if (boresight != null)
             {
-                boresight.position = Camera.main.WorldToScreenPoint(controller.BoresightPos);
+                boresight.position = playerCam.WorldToScreenPoint(controller.BoresightPos);
                 boresight.gameObject.SetActive(boresight.position.z > 1f);
             }
 
             if (mousePos != null)
             {
-                mousePos.position = Camera.main.WorldToScreenPoint(controller.MouseAimPos);
+                mousePos.position = playerCam.WorldToScreenPoint(controller.MouseAimPos);
                 mousePos.gameObject.SetActive(mousePos.position.z > 1f);
             }
         }
